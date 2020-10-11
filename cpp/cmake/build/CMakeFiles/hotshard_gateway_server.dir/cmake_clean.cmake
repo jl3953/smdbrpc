49abf60b17ea9671831100e5093ac2ce5ed0f099
Jennifer Lam <jl87@princeton.edu>
2020-10-11 00:17:51 -0400

    Add more cpp build files

---
+++ cpp/cmake/build/CMakeFiles/hotshard_gateway_server.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hotshard_gateway_server.dir/hotshard_gateway_server.cc.o"
+  "CMakeFiles/hotshard_gateway_server.dir/smdbrpc.grpc.pb.cc.o"
+  "CMakeFiles/hotshard_gateway_server.dir/smdbrpc.pb.cc.o"
+  "hotshard_gateway_server"
+  "hotshard_gateway_server.pdb"
+  "smdbrpc.grpc.pb.cc"
+  "smdbrpc.grpc.pb.h"
+  "smdbrpc.pb.cc"
+  "smdbrpc.pb.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hotshard_gateway_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
