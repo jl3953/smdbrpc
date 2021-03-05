0debe5e3c407fb5607e18974488952ccac8ba1b3
Jennifer Lam <jl87@princeton.edu>
2021-03-05 05:39:35 -0500

    test suite

--- go/hotshard_gateway_client/client.go
+++ go/hotshard_gateway_client/client.go
@@ -22,8 +22,6 @@ package main
 import (
 	"context"
 	"log"
-	"os"
-	"strconv"
 	"time"
 
 	"google.golang.org/grpc"
@@ -33,6 +31,30 @@ import (
 const (
 	address = "localhost:50051"
 )
+
+func makeRequest(writeset []*smdbrpc.KVPair,
+	readset []uint64,
+	walltime_ptr *int64,
+	logicaltime_ptr *int32) smdbrpc.HotshardRequest {
+
+	request := smdbrpc.HotshardRequest{
+		Hlctimestamp: &smdbrpc.HLCTimestamp{
+			Walltime:    walltime_ptr,
+			Logicaltime: logicaltime_ptr,
+		},
+		WriteKeyset: writeset,
+		ReadKeyset:  readset,
+	}
+
+	return request
+}
+
+func makeKVPair(key_ptr *uint64, val_ptr *uint64) smdbrpc.KVPair {
+	return smdbrpc.KVPair{
+		Key:   key_ptr,
+		Value: val_ptr,
+	}
+}
 
 func main() {
 	// Set up a connection to the server.
@@ -48,57 +70,266 @@ func main() {
 	defer cancel()
 
 	var walltime int64 = time.Now().UnixNano()
-	var logicaltime int32 = 214
-	request := smdbrpc.HotshardRequest{
-		Hlctimestamp: &smdbrpc.HLCTimestamp{
-			Walltime: &walltime,
-			Logicaltime: &logicaltime,
-		},
-		WriteKeyset: make([]*smdbrpc.KVPair, 0),
-		ReadKeyset: make([]uint64, 0),
-	}
+	var logicaltime int32 = 0 /* jenndebug this might be a problem */
 
-	for i := 1; i < len(os.Args); i++ {
-		log.Printf("jenndebug os.Args[%d]:%+v\n", i, os.Args[i])
-		temp, _ := strconv.Atoi(os.Args[i])
-		var key = uint64(temp)
-		temp, _ = strconv.Atoi(os.Args[i])
-		var value = uint64(temp)
+	var key, val uint64 = 1994214, 1994214
+	kvpair := makeKVPair(&key, &val)
+	writeset := []*smdbrpc.KVPair{&kvpair}
 
-		kvPair := smdbrpc.KVPair{
-			Key:   &key,
-			Value: &value,
-		}
-		request.WriteKeyset = append(request.WriteKeyset, &kvPair)
-		request.ReadKeyset = append(request.ReadKeyset, key)
-	}
-
-	//var jennbday, christmas uint64 = 1994214, 1225
-
-	r, err := c.ContactHotshard(
-		ctx, &request,
-		//&smdbrpc.HotshardRequest{
-		//	Hlctimestamp: &smdbrpc.HLCTimestamp{
-		//		Walltime: &walltime,
-		//	},
-		//	WriteKeyset: []*smdbrpc.KVPair{
-		//		{
-		//			Key:   &jennbday,
-		//			Value: &jennbday,
-		//		}, {
-		//			Key:   &christmas,
-		//			Value: &christmas,
-		//		},
-		//	},
-		//	ReadKeyset: []uint64{1994214, 1225},
-		//},
-	)
+	/* Case 1:
+	txn1: write(1994214) = 1994214, ts = now
+	txn2: write(1994214) = 2020, ts = now + 1
+	txn3: read(1994214) = 2020, ts = now + 2
+	 */
+	request := makeRequest(writeset, nil, &walltime, &logicaltime)
+	r, err := c.ContactHotshard(ctx, &request)
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
+	} else if *r.IsCommitted == false {
+		log.Printf("isCommitted:[%+v]\n", *r.IsCommitted)
 	}
-	log.Printf("isCommitted:[%+v]\n", *r.IsCommitted)
+
+	walltime2 := walltime + 1
+	val = 2020
+	kvpair = makeKVPair(&key, &val)
+	writeset = []*smdbrpc.KVPair{&kvpair}
+	request = makeRequest(writeset, nil, &walltime2, &logicaltime)
+	r, err = c.ContactHotshard(ctx, &request)
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	} else if *r.IsCommitted == false {
+		log.Printf("isCommitted:[%+v]\n", *r.IsCommitted)
+	}
+
+	walltime3 := walltime + 2
+	readset := []uint64{key}
+	request = makeRequest(nil, readset, &walltime3, &logicaltime)
+	r, err = c.ContactHotshard(ctx, &request)
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	} else if *r.IsCommitted == false {
+		log.Printf("isCommitted:[%+v]\n", *r.IsCommitted)
+	}
 	for _, kvPair := range r.ReadValueset {
 		log.Printf("key:[%+v], val:[%+v]\n",
 			*kvPair.Key, *kvPair.Value)
 	}
+	log.Printf("expected: (1994214, 2020)\n")
+
+	/* Case 2:
+	txn1: write(1994214) = 1994214, ts = now
+	txn2: write(1994214) = 2020, ts = now + 5
+	txn3: read(1994214) = 1994214, ts = now + 2
+	*/
+	walltime, logicaltime = time.Now().UnixNano(), 0
+	key, val = 1994214, 1994214
+	kvpair = makeKVPair(&key, &val)
+	writeset = []*smdbrpc.KVPair{&kvpair}
+	request = makeRequest(writeset, nil, &walltime, &logicaltime)
+	r, err = c.ContactHotshard(ctx, &request)
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	} else if *r.IsCommitted == false {
+		log.Printf("isCommitted:[%+v]\n", *r.IsCommitted)
+	}
+
+	walltime2 = walltime + 5
+	val = 2020
+	kvpair = makeKVPair(&key, &val)
+	writeset = []*smdbrpc.KVPair{&kvpair}
+	request = makeRequest(writeset, nil, &walltime2, &logicaltime)
+	r, err = c.ContactHotshard(ctx, &request)
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	} else if *r.IsCommitted == false {
+		log.Printf("isCommitted:[%+v]\n", *r.IsCommitted)
+	}
+
+	walltime3 = walltime + 2
+	readset = []uint64{key}
+	request = makeRequest(nil, readset, &walltime3, &logicaltime)
+	r, err = c.ContactHotshard(ctx, &request)
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	} else if *r.IsCommitted == false {
+		log.Printf("isCommitted:[%+v]\n", *r.IsCommitted)
+	}
+	for _, kvPair := range r.ReadValueset {
+		log.Printf("key:[%+v], val:[%+v]\n",
+			*kvPair.Key, *kvPair.Value)
+	}
+	log.Printf("expected: (1994214, 1994214)\n")
+
+	/* Case 3:
+	txn1: write(1994214) = 1994214, ts = now
+	txn2: write(1994214) = 2020, ts = now - 10 FAIL
+	txn3: read(1994214) = 1994214, ts = now + 1
+	*/
+	walltime, logicaltime = time.Now().UnixNano(), 0
+	key, val = 1994214, 1994214
+	kvpair = makeKVPair(&key, &val)
+	writeset = []*smdbrpc.KVPair{&kvpair}
+	request = makeRequest(writeset, nil, &walltime, &logicaltime)
+	r, err = c.ContactHotshard(ctx, &request)
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	} else if *r.IsCommitted == false {
+		log.Printf("isCommitted:[%+v]\n", *r.IsCommitted)
+	}
+
+	walltime2 = walltime - 10
+	val = 2020
+	kvpair = makeKVPair(&key, &val)
+	writeset = []*smdbrpc.KVPair{&kvpair}
+	request = makeRequest(writeset, nil, &walltime2, &logicaltime)
+	r, err = c.ContactHotshard(ctx, &request)
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("isCommitted:[%+v]\n", *r.IsCommitted)
+	log.Printf("EXPECTED: false\n")
+
+	walltime3 = walltime + 2
+	readset = []uint64{key}
+	request = makeRequest(nil, readset, &walltime3, &logicaltime)
+	r, err = c.ContactHotshard(ctx, &request)
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	} else if *r.IsCommitted == false {
+		log.Printf("isCommitted:[%+v]\n", *r.IsCommitted)
+	}
+	for _, kvPair := range r.ReadValueset {
+		log.Printf("key:[%+v], val:[%+v]\n",
+			*kvPair.Key, *kvPair.Value)
+	}
+	log.Printf("expected: (1994214, 1994214)\n")
+
+	/* Case 4:
+	txn1: write(1994214) = 1994214, ts = now, 0
+	txn2: write(1994214) = 2020, ts = now, 1
+	txn3: read(1994214) = 2020, ts = now, 2
+	*/
+	walltime, logicaltime = time.Now().UnixNano(), 0
+	request = makeRequest(writeset, nil, &walltime, &logicaltime)
+	r, err = c.ContactHotshard(ctx, &request)
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	} else if *r.IsCommitted == false {
+		log.Printf("isCommitted:[%+v]\n", *r.IsCommitted)
+	}
+
+	logicaltime2 := logicaltime + 1
+	val = 2020
+	kvpair = makeKVPair(&key, &val)
+	writeset = []*smdbrpc.KVPair{&kvpair}
+	request = makeRequest(writeset, nil, &walltime, &logicaltime2)
+	r, err = c.ContactHotshard(ctx, &request)
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	} else if *r.IsCommitted == false {
+		log.Printf("isCommitted:[%+v]\n", *r.IsCommitted)
+	}
+
+	logicaltime3 := logicaltime + 2
+	readset = []uint64{key}
+	request = makeRequest(nil, readset, &walltime, &logicaltime3)
+	r, err = c.ContactHotshard(ctx, &request)
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	} else if *r.IsCommitted == false {
+		log.Printf("isCommitted:[%+v]\n", *r.IsCommitted)
+	}
+	for _, kvPair := range r.ReadValueset {
+		log.Printf("key:[%+v], val:[%+v]\n",
+			*kvPair.Key, *kvPair.Value)
+	}
+	log.Printf("expected: (1994214, 2020)\n")
+
+	/* Case 5:
+	txn1: write(1994214) = 1994214, ts = now, 0
+	txn2: write(1994214) = 2020, ts = now, 10
+	txn3: read(1994214) = 1994214, ts = now, 5
+	*/
+	walltime, logicaltime = time.Now().UnixNano(), 0
+	key, val = 1994214, 1994214
+	kvpair = makeKVPair(&key, &val)
+	writeset = []*smdbrpc.KVPair{&kvpair}
+	request = makeRequest(writeset, nil, &walltime, &logicaltime)
+	r, err = c.ContactHotshard(ctx, &request)
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	} else if *r.IsCommitted == false {
+		log.Printf("isCommitted:[%+v]\n", *r.IsCommitted)
+	}
+
+	logicaltime2 = logicaltime + 10
+	val = 2020
+	kvpair = makeKVPair(&key, &val)
+	writeset = []*smdbrpc.KVPair{&kvpair}
+	request = makeRequest(writeset, nil, &walltime, &logicaltime2)
+	r, err = c.ContactHotshard(ctx, &request)
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	} else if *r.IsCommitted == false {
+		log.Printf("isCommitted:[%+v]\n", *r.IsCommitted)
+	}
+
+	logicaltime3 = logicaltime + 5
+	readset = []uint64{key}
+	request = makeRequest(nil, readset, &walltime, &logicaltime3)
+	r, err = c.ContactHotshard(ctx, &request)
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	} else if *r.IsCommitted == false {
+		log.Printf("isCommitted:[%+v]\n", *r.IsCommitted)
+	}
+	for _, kvPair := range r.ReadValueset {
+		log.Printf("key:[%+v], val:[%+v]\n",
+			*kvPair.Key, *kvPair.Value)
+	}
+	log.Printf("expected: (1994214, 1994214)\n")
+
+	/* Case 6:
+	txn1: write(1994214) = 1994214, ts = now, 100
+	txn2: write(1994214) = 2020, ts = now, 90 FAIL
+	txn3: read(1994214) = 1994214, ts = now, 101
+	*/
+	walltime, logicaltime = time.Now().UnixNano(), 100
+	key, val = 1994214, 1994214
+	kvpair = makeKVPair(&key, &val)
+	writeset = []*smdbrpc.KVPair{&kvpair}
+	request = makeRequest(writeset, nil, &walltime, &logicaltime)
+	r, err = c.ContactHotshard(ctx, &request)
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	} else if *r.IsCommitted == false {
+		log.Printf("isCommitted:[%+v]\n", *r.IsCommitted)
+	}
+
+	logicaltime2 = logicaltime - 10
+	val = 2020
+	kvpair = makeKVPair(&key, &val)
+	writeset = []*smdbrpc.KVPair{&kvpair}
+	request = makeRequest(writeset, nil, &walltime, &logicaltime2)
+	r, err = c.ContactHotshard(ctx, &request)
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("isCommitted:[%+v]\n", *r.IsCommitted)
+	log.Printf("EXPECTED: false\n")
+
+	logicaltime3 = logicaltime + 2
+	readset = []uint64{key}
+	request = makeRequest(nil, readset, &walltime, &logicaltime3)
+	r, err = c.ContactHotshard(ctx, &request)
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	} else if *r.IsCommitted == false {
+		log.Printf("isCommitted:[%+v]\n", *r.IsCommitted)
+	}
+	for _, kvPair := range r.ReadValueset {
+		log.Printf("key:[%+v], val:[%+v]\n",
+			*kvPair.Key, *kvPair.Value)
+	}
+	log.Printf("expected: (1994214, 1994214)\n")
 }
