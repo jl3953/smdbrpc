dbba372806eace95cb318bfd8b2dfbf7f0162559
Jennifer Lam <jl87@princeton.edu>
2021-08-22 19:58:42 -0400

    update generate protos file

--- generate_new_protos.sh
+++ generate_new_protos.sh
@@ -3,9 +3,14 @@
 cd ~/smdbrpc/protos
 protoc --go_out=../go/build/gen --go-grpc_out=../go/build/gen *.proto
 
+cd ~/smdbrpc/protos
+python3 -m grpc_tools.protoc -I. --python_out=../python --grpc_python_out=../python *.proto
+
 cd ~/smdbrpc/cpp
 rm -rf cmake/build
 mkdir -p cmake/build
 pushd cmake/build
 cmake -DCMAKE_PREFIX_PATH=$MY_INSTALL_PATH ../..
 make -j
+
+
