653f6467384b76f788ec3afcb2408ec4d66a2d82
Jennifer Lam <jl87@princeton.edu>
2022-01-04 00:11:54 -0500

    debug

--- go/hotshard_gateway_client/manual_promotion.go
+++ go/hotshard_gateway_client/manual_promotion.go
@@ -285,11 +285,6 @@ func main() {
 		for i := int64(0); i < *keyMax-*keyMin; i++ {
 			keys[i] = transformKey(i + *keyMin, *keyspace,
 				*hash_randomize_keyspace, *enable_fixed_sized_encoding)
-			log.Fatalf("keys[%d]=%d, keyMin %d, keyspace %d, " +
-				"hash_randomize_keyspace %v, " +
-				"enable_fixed_sized_encoding %v\n", i, keys[i], *keyMin,
-				*keyspace, *hash_randomize_keyspace,
-				*enable_fixed_sized_encoding)
 		}
 		sort.Slice(keys, func(i, j int) bool {
 			return keys[i] < keys[j]
